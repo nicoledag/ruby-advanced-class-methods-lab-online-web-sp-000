233c4c31e9f96248b1b971a7ee5c9ad2ac22b1ce
Nicole D <nnardella@gmail.com>
2018-11-15 15:45:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,6 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    binding.pry
     split_filename = filename.chomp(".mp3").split (" - ")
     song = self.new
     song.name = split_filename[1]
